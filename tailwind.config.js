ad1f24f969f391ca5945e9035ffa9f69d60b49a7
Van <vannb.fu@gmail.com>
2024-01-13 22:35:01 +0700

    UX UI change

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        lg: "0px 8px 16px 0px rgba(171, 190, 209, 0.40)",
+      },
+      colors: {
+        primary: "#2A6BAC",
+        silver: "#F5F7FA",
+        dark: "#263238",
+        blue: "#ABBED1",
+        grey: "#4D4D4D",
+        black: "#18191F",
+        white: "#FFFFFF",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "2rem",
+        md: "0rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
+  
